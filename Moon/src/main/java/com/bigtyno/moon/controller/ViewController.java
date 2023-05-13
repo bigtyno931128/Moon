7decc1950ce6cc6b9228ac23f2e2a8fed207e3b2
bigtyno931128 <bigtyno4@gmail.com>
2023-05-14 01:30:41 +0900

    #13-일정목록 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.

---
+++ Moon/src/main/java/com/bigtyno/moon/controller/ViewController.java
@@ -0,0 +1,7 @@
+package com.bigtyno.moon.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ViewController {
+}
---
+++ Moon/src/test/java/com/bigtyno/moon/controller/ViewControllerTest.java
@@ -0,0 +1,52 @@
+package com.bigtyno.moon.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러")
+@WebMvcTest(ViewController.class)
+class ViewControllerTest {
+    private final MockMvc mvc;
+    public ViewControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    public void 일정목록_페이지() throws Exception {
+
+        mvc.perform(get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("posts"));
+
+    }
+
+    @Test
+    public void 일정상세_페이지() throws Exception {
+
+        mvc.perform(get("/posts/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("posts/1"))
+                .andExpect(model().attributeExists("post"))
+                .andExpect(model().attributeExists("postComments"));
+
+    }
+
+    @Test
+    public void 할일_검색_전용페이지() throws Exception {
+
+        mvc.perform(get("/posts/Search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+
+    }
+
+}
