7d0ee64c66661c903ba0b38923d644d0d66cf800
bigtyno931128 <bigtyno4@gmail.com>
2023-05-18 13:20:15 +0900

    #20 - 할일 목록 api
    할일 목록 api 와 testcode 를 구현하였으며 , 추가적으로 업데이트 되어야 할 부분들이 존재하는것 같다 .

--- Moon/src/main/java/com/bigtyno/moon/configurarion/frilter/JwtTokenFilter.java
+++ Moon/src/main/java/com/bigtyno/moon/configurarion/frilter/JwtTokenFilter.java
@@ -11,13 +11,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 
 @Slf4j
