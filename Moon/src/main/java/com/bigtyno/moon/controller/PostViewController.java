38dd48f3a32ce9240ab8b7226be9661e8a54689b
bigtyno931128 <bigtyno4@gmail.com>
2023-05-22 16:21:49 +0900

    # 30 - 페이지와 api 연동
    기존에 만들었던 페이지와 구현했던 api 연동 
    - 작업목록 페이지와 작업페이지 연동
    
    페이지를 타임리프로 구현하기로 하였으며 
    기존의 restapi 를 구현할때 사용한 컨트롤러를 놔두고 
    view이름이 들어간 컨트롤러를 통해 따로 구현해주었다 .

---
+++ Moon/src/main/java/com/bigtyno/moon/controller/PostViewController.java
@@ -0,0 +1,49 @@
+package com.bigtyno.moon.controller;
+
+import com.bigtyno.moon.controller.response.CommentResponse;
+import com.bigtyno.moon.controller.response.PostResponse;
+import com.bigtyno.moon.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class PostViewController {
+
+    private final PostService postService;
+
+    // 할일 목록
+    @GetMapping("/posts")
+    public String posts(
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+    ) {
+        Page<PostResponse> posts = postService.list(pageable).map(PostResponse::fromPost);
+
+        map.addAttribute("posts", posts);
+
+
+        return "posts/index";
+    }
+
+    // 할일 ( 상세 페이지 )
+    @GetMapping("/posts/{postId}")
+    public String post(@PathVariable Long postId, ModelMap map ,
+                       @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        PostResponse post = PostResponse.fromPost(postService.detail(postId));
+        map.addAttribute("post", post);
+        map.addAttribute("postComments",postService.getComments(postId, pageable).map(CommentResponse::fromComment));
+
+        return "posts/detail";
+    }
+}
--- Moon/src/main/java/com/bigtyno/moon/controller/ViewController.java
+++ Moon/src/main/java/com/bigtyno/moon/controller/ViewController.java
@@ -10,19 +10,9 @@ import java.util.List;
 @Controller
 public class ViewController {
 
-    // 할일 목록
-    @GetMapping("/posts")
-    public String posts(ModelMap map) {
-        map.addAttribute("posts", List.of());
-        return "posts/index";
+    @GetMapping("/")
+    public String root() {
+        return "forward:/posts/index";
     }
 
-    // 할일 ( 상세 페이지 )
-    @GetMapping("/posts/{postId}")
-    public String post(@PathVariable Long postId, ModelMap map ) {
-        map.addAttribute("post","post");
-        map.addAttribute("postComments",List.of());
-
-        return "posts/detail";
-    }
 }
